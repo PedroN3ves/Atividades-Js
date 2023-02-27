34f30bec08765ff3655b662db985a5623b923aab
Pedro Neves <111977651+PedroN3ves@users.noreply.github.com>
2023-02-27 11:00:46 -0300

    Add files via upload

--- js/atividade03.js
+++ js/atividade03.js
@@ -4,7 +4,7 @@ let name = prompt("Digite seu nome: ");
 let age = parseInt(prompt("digite sua idade: "));
 let yearsWorked = parseInt(prompt("Quantos anos você tem de Contribuição: "));
 let gender = prompt("Escolha seu gênero: M (masculino) ou F (feminino): ");
-
+let retirement = age + yearsWorked;
 
 console.log("Seu nome:", name);
 console.log("Sua idade:", age, "anos");
@@ -14,24 +14,16 @@ console.log("Seu tempo de contribuição é:", yearsWorked, "anos");
 if(age < 0 || yearsWorked < 0){
     console.log("Algo deu errado, corrija suas informações");
 }else if(gender == "M" || "m"){
-    if(yearsWorked >= 15){
-        if(age >= 65){
+    if(retirement >= 95){
             console.log("Parabéns, você está apto(a) para se aposentar!");
-        }else{
-            console.log("Você precisa trabalhar ainda mais", 65-age, "anos para se aposentar");
-        }
     } else{
-        console.log("Você precisa trabalhar ainda mais", 15-yearsWorked, "anos pra se aposentar");
+        console.log("Você precisa trabalhar ainda mais para se aposentar");
     }
 }else if(gender == "F" || "f"){
-    if(yearsWorked >= 15){
-        if(age >= 62){
+    if(retirement >= 90){
             console.log("Parabéns, você está apto(a) para se aposentar!");
-        }else{
-            console.log("Você precisa trabalhar ainda mais", 62-age, "anos para se aposentar");
-        }
     } else{
-        console.log("Você precisa trabalhar ainda mais", 15-yearsWorked, "anos pra se aposentar");
+        console.log("Você precisa trabalhar ainda mais para se aposentar");
     }
 }else{
     console.log("Algo deu errado, corrija suas informações");
